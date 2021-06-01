f50df8c928eed6e2f218088ddc5e8db81d1b121c
Osmar Coronel <osmar@Osmars-MacBook-Pro.local>
2021-05-31 21:25:18 -0600

    Byebug

--- lib/google_places/client.rb
+++ lib/google_places/client.rb
@@ -167,6 +167,7 @@ module GooglePlaces
     end
 
     def spots_by_find_query(query, options = {})
+      byebug
       options = @options.merge(options)
       detail = options.delete(:detail)
       collection_detail_level(
